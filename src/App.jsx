5dd95394fee3b82231c04270abff2b91bc75a804
rubis-design <rubis.smm@gmail.com>
2024-10-14 16:35:28 +0300

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,24 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Главная</Link>
-            </li>
-            <li>
-              <Link to="/about">О нас</Link>
-            </li>
-            <li>
-              <Link to="/contact">Контакты</Link>
-            </li>
-            <li>
-              <Link to="/login">Войти</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Главная</Link>
+          </li>
+          <li>
+            <Link to="/about">О нас</Link>
+          </li>
+          <li>
+            <Link to="/contact">Контакты</Link>
+          </li>
+          <li>
+            <Link to="/login">Войти</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
